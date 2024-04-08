32af416bd6de54ee59b68777843c3f97f7bba093
Mahir Abrar <hamid.mahir2597@Yahoo.com>
2024-04-08 15:34:28 +1000

    Incorporate flask

--- app.py
+++ app.py
@@ -2,7 +2,20 @@
 import pandas as pd
 import ast
 import priceElasticityModel
+from flask import Flask, jsonify
+from flask_cors import CORS
 
+app = Flask(__name__)
+CORS(app)
+
+# get all items
+@app.route('/getItems', methods=['GET'])
+def getItems():
+    salesDF = pd.read_csv("sales.csv")
+    items = salesDF['item_id'].unique()
+    return jsonify(items.tolist())
+
+@app.route('/getPriceElasticity', methods=['GET'])
 def main():
     salesDF = pd.read_csv("sales.csv")
     priceDF = pd.read_csv("price.csv")
@@ -19,10 +32,13 @@ def main():
     item_id = "FOODS_1_073"
     
     model = priceElasticityModel.createModel(salesDF, priceDF, year, store_id, item_id)
-    # print(model)
     return model
-    
 
+# for testing with frontend, comment the next2 lines
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+# For testing with frontend
 if __name__ == '__main__':
     main()
-    
