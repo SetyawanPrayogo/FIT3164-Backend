73513b74739f70f71c597afcd3f41d39a62c0259
Setyawan Prayogo <spra0039@student.monash.edu>
2024-04-08 20:19:40 +1000

    Fixed some unneeded line and put it in seperate function

--- priceElasticityModel.py
+++ priceElasticityModel.py
@@ -20,9 +20,85 @@ def createModel(salesDF, priceDF, year: int, store_id: str, item_id: str, deg: i
         # Improvement: put minimum year that is valid
         return "Year Invalid"
 
+    # Choose the level of the model
     level = levelSelection(priceDF, year, store_id, item_id)
     print(level)
     
+    # get data for model
+    dataModel = getData(year-1)
+    
+    # get the productInfo (state_id, store_id, cat_id, dept_id, item_id)
+    productInfo = getProductInfo(store_id, item_id)
+    
+    # filter the data based on the level
+    dataModel = filterData(dataModel, productInfo, level)
+    # print(dataModel.shape)
+    
+    x = dataModel['price_discount']
+    x = x.sort_values()
+    y = dataModel['demand_percent']
+
+    # Generate values 0 to 100 by 0.01
+    x_values = np.arange(0, 100.01, 0.01)
+    
+    # Degree ?? - To think
+    poly = PolynomialFeatures(degree=deg, include_bias=False)
+    X = poly.fit_transform(x.to_numpy().reshape(-1, 1))
+    poly_reg_model = LinearRegression()
+    poly_reg_model.fit(X, y)
+    
+    # Prediction
+    x_values_poly = poly.fit_transform(x_values.reshape(-1, 1))
+    y_predicted = poly_reg_model.predict(x_values_poly)
+    
+    # Calcualte RMSE
+    actual_y_predicted = poly_reg_model.predict(X)
+    rmse = np.sqrt(mean_squared_error(y, actual_y_predicted))
+    print("RMSE:", rmse)
+
+    plt.figure(figsize=(10, 6))
+    plt.scatter(x, y, label='Actual Data')
+    plt.plot(x_values, y_predicted, c="red", label='Polynomial Regression')
+    plt.xlabel('Price Discount (%)')
+    plt.ylabel('Change in Demand (%)')
+    plt.legend()
+    plt.show()
+            
+    return poly, poly_reg_model, rmse
+
+def predictDemand(poly, model, discount):
+    x_predict_poly = poly.fit_transform(np.array(discount).reshape(-1, 1))
+    y_predict = model.predict(x_predict_poly)
+    
+    return y_predict[0]
+
+def getData(year):
+    filePath = f"modelData/data_{year}.csv"
+    data = pd.read_csv(filePath)
+    return data
+
+def filterData(data, productInfo, level: str):
+    if level in ["item_id", "dept_id", "cat_id"]:
+        store_id = productInfo.get("store_id")
+        filter_product = productInfo.get(level)
+        filter_value = "store_id == " + f"'{store_id}'" + " and " + f"{level} == '{filter_product}'"
+        # print(filter_value)
+    else: 
+        filter_product = productInfo.get(level)
+        filter_value = f"{level} == '{filter_product}'"
+    
+    data = data.query(filter_value).reset_index(drop=True)
+    
+    return data
+
+def getBase(salesDF, priceDF, year: int, store_id: str, item_id: str):
+    productPriceInfo = priceDF[(priceDF['store_id'] == store_id) & (priceDF['item_id'] == item_id)].reset_index()
+    productSalesInfo = salesDF[(salesDF['item_id'] == item_id) & (salesDF['store_id'] == store_id)].reset_index()
+    
+    if productPriceInfo['Price Count'].iloc[0][year-1] == 0: 
+        # Improvement: put minimum year that is valid
+        return "Year Invalid"
+
     base_price = productPriceInfo['Base Price'][0][year-1]
     # print("base price: ")
     # print(base_price)
@@ -43,62 +119,8 @@ def createModel(salesDF, priceDF, year: int, store_id: str, item_id: str, deg: i
     # print("base demand: ")
     # print(base_demand)
     
-    dataModel = getData(year-1)
+    return base_price, base_demand
     
-    productInfo = getProductInfo(store_id, item_id)
-    dataModel = filterData(dataModel, productInfo, level)
-    print(dataModel.shape)
-    
-    x = dataModel['price_discount']
-    x = x.sort_values()
-    y = dataModel['demand_percent']
-
-    # Generate values 0 to 100 by 0.01
-    x_values = np.arange(0, 100.01, 0.01)
-    # Degree ?? - To think
-    poly = PolynomialFeatures(degree=deg, include_bias=False)
-    actual_poly_features = poly.fit_transform(x.to_numpy().reshape(-1, 1))
-    poly_features = poly.fit_transform(x_values.reshape(-1, 1))
-
-    poly_reg_model = LinearRegression()
-    poly_reg_model.fit(actual_poly_features, y)
-    
-    y_predicted = poly_reg_model.predict(poly_features)
-    actual_y_predicted = poly_reg_model.predict(actual_poly_features)
-    
-    # Calculate rmse
-    mse = mean_squared_error(y, actual_y_predicted)
-    rmse = np.sqrt(mse)
-    print(rmse)
-
-    plt.figure(figsize=(10, 6))
-    plt.scatter(x, y)
-    plt.plot(x_values, y_predicted, c="red")
-    # plt.yscale('log')
-    # plt.gca().yaxis.set_major_formatter(ScalarFormatter())
-            
-    return y_predicted
-
-def getData(year):
-    filePath = f"modelData/data_{year}.csv"
-    data = pd.read_csv(filePath)
-    return data
-
-def filterData(data, productInfo, level: str):
-    
-    if level in ["item_id", "dept_id", "cat_id"]:
-        store_id = productInfo.get("store_id")
-        filter_product = productInfo.get(level)
-        filter_value = "store_id == " + f"'{store_id}'" + " and " + f"{level} == '{filter_product}'"
-        # print(filter_value)
-    else: 
-        filter_product = productInfo.get(level)
-        filter_value = f"{level} == '{filter_product}'"
-    
-    data = data.query(filter_value).reset_index(drop=True)
-    
-    return data
-
 if __name__ == '__main__':
     salesDF = pd.read_csv("sales.csv")
     priceDF = pd.read_csv("price.csv")
@@ -114,5 +136,8 @@ if __name__ == '__main__':
     store_id = "TX_1"
     item_id = "HOUSEHOLD_2_466"
     
-    y_predicted = createModel(salesDF, priceDF, year, store_id, item_id, deg = 2)
-    plt.show()
+    base_price, base_demand = getBase(salesDF, priceDF, year, store_id, item_id)
+    
+    poly, model, rmse = createModel(salesDF, priceDF, year, store_id, item_id, deg = 3)
+
+    y_predict = predictDemand(poly, model, 60)
